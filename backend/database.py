0b1dd1cc8bb8b0b429608c9e1696f25f3814e76d
root <root@Yash.localdomain>
2026-05-15 13:25:46 +0530

    updated the env in code

--- backend/database.py
+++ backend/database.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "tasks_db")
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
+DB_HOST = "172.17.0.1"
+DB_PORT = "5432"
+DB_NAME = "tasks_db"
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
